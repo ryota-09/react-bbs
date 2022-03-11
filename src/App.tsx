094e306dce7f340dabf515ee23051933cf38491d
ryota-09 <n.ryo950911@gmail.com>
2022-03-11 09:57:19 +0900

    article型の設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [articleList, setArticleList] = useState([])
   return (
     <div className="App">
       <h1>掲示板アプリ</h1>
